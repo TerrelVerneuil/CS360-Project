1632ba640808f20ba3ae523ccd42434d89570ae4
llanjo01 <151190891+llanjo01@users.noreply.github.com>
2023-11-27 17:49:01 -0500

    Update util.php

--- project files/util.php
+++ project files/util.php
@@ -47,6 +47,39 @@ function showTryAgain($db){
   <?php
 }
 
+function showSellForm($db){
+  ?>
+  <DIV class="main-content">
+    <FORM name='fmListItem' action='?op=listedItem' method='POST'>
+    <P>Please enter the information of the item you wish to sell.</P>
+    <P>
+    Name of Item:  
+    <INPUT type='text' name='name'      placeholder='Item Name' />
+    </P>
+    <P>
+    Price of Item: 
+    <INPUT type='text' name='price'     placeholder='Item Price' />
+    </P>
+    <P>
+    Brief Description of Item:  
+    <INPUT type='text' name='desc'      placeholder='Item Description' />
+    </P>
+    <P>
+    Select a Category for your Item:  
+    <SELECT name="category">
+      <OPTION value="Electronics">Electronics</OPTION>
+      <OPTION value="Home Goods">Home Goods</OPTION>
+      <OPTION value="Kitchen Ware">Kitchen Ware</OPTION>
+      <OPTION value="Toys & Games">Toys & Games</OPTION>
+    </SELECT>
+    </P>
+    <P>
+    <INPUT type='submit' value='List Item'/>
+    </P>
+    </FORM>
+  </DIV>
+  <?php
+}
 
 function getName($db, $uid){
 	$sql = "SELECT name 
@@ -71,4 +104,60 @@ function showLogoutForm() {
         </FORM>
     <?php
     }
+
+function showFilteredItems($db, $filters){
+  echo("<DIV class='main-content'><H1>Items that fit your search</H1>");
+
+  $search = $filters['searchbar'];
+
+  
+  $query = "SELECT *
+            FROM item
+            WHERE name LIKE '%$search%'";
+
+  $condition = "WHERE ";
+
+  for ($x = 1; $x <= 4; $x++) {
+    if (isset($_POST['cb'.$x])) {
+      $filter = $_POST['cb'.$x];
+      $condition = $condition . "itemCat = '$filter' OR ";
+    }
+  }
+
+  if($condition != "WHERE "){
+    $condition = substr_replace($condition, "", -4) . ";";
+    $query = "SELECT description, price FROM (" . $query . ") AS searched " . $condition;
+  }
+
+  $res = $db->query($query);
+  
+  //echo("<DIV class='main-content'><H1>".$query."</H1></DIV>");
+
+  if($res != FALSE){
+    while($row = $res->fetch()){
+      echo("<DIV class='item'>");
+      echo("<DIV class='item-box'>Null</DIV>");
+      echo("<P>".$row['description']."</P>");
+      echo("<BR>");                               //figure out how to create a new line.
+      echo("<P>Price:".$row['price']."</P>");
+      echo("</DIV>");
+    }
+  }
+
+  echo("</DIV>");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
   ?>
