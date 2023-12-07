3d5c69c852c67686f98d962a6b9a604f1b56daf4
T404 <44128398+TerrelVerneuil@users.noreply.github.com>
2023-12-06 20:31:36 -0500

    Update util.php

--- project files/util.php
+++ project files/util.php
@@ -3,7 +3,45 @@ session_start();
 include_once("db_connect.php");
 //we connect to database here
 //include_once("db_connect.php");
+function showAccountManagementForm($db, $uid){
+?>
+    <link rel="stylesheet" href="dashboard.css">
+    <div class="main-content">
+	<!-- there should be a button that goes back to the dashboard.php -->
+	<div class="back-button-container">
+            <a href="dashboard.php" class="back-button">Back to Dashboard</a>
+        </div>       
+	<div class="account-container">
+            <h2>Welcome <?php echo getName($db, $uid) ?>!</h2>
+            <form name='actManage' method='POST' action='dashboard.php?op=Account_Manage' class="login-form">
+                <h2>Account Management</h2>
+		<!-- should have orders tab, wishlist addresses -->
+		
+	<!-- Personal Information Section -->
+                <div class="manage-section">
+                    <h3>Personal Information</h3>
+		<!-- display uid and name -->
+		<p>Name: <?php echo getName($db, $uid) ?></p>
+		<p>User ID: <?php echo $uid ?></p>
+                <!-- Change Password Section -->
+                <div class="manage-section">
+                    <h3>Change Password</h3>
+                    <input type="password" name="currentPassword" placeholder="Current Password" class="form-input"/>
+                    <input type="password" name="newPassword" placeholder="New Password" class="form-input"/>
+                    <input type="password" name="confirmNewPassword" placeholder="Confirm New Password" class="form-input"/>
+                </div>
 
+                <input type="submit" value="Update Account" class="login-button"/>
+            </form>
+        </div>
+    </div>
+
+    <?php
+}
+   
+    
+
+?>
 function handleLoginForm($db, $dest){
 ?>
     <link rel="stylesheet" href="dashboard.css">
@@ -291,7 +329,33 @@ function showCart($db, $uid){
 
 
 }
+function validateNewPassword($db, $uid,$current_password, $newPassword, $confirmPassword){
+	$sql = "SELECT password 
+		FROM shop_user
+		WHERE userID=$uid";
+	$res = $db->query($sql);
+	
+	if($res != FALSE && $res-> rowCount() == 1){
+		$row = $res -> fetch();
+		$curr_password = $row['password'];
+		if($curr_password === $current_password){
+			if($newPassword !== $curr_password&&$newPassword ===$confirmPassword){
+				echo "Successfully Changed Password";
+				showAccountManagementForm($db, $uid);
+				return true;
+			}else{
+				echo "New Password & Confirm New Password fields must match";
+				showAccountManagementForm($db, $uid);
+				return false;
+			}
+		}else{
+			echo "invalid current password";
+			showAccountManagementForm($db, $uid);
+			return false;
+		}
 
+	}	
+}
 function cannotAddToCart($error){
   echo("<DIV class='main-content'>\n");
   echo("<P>$error</P>");
