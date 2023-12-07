4871eafa5e9b03f1858bf8ab633c6dee70bd0bf3
T404 <44128398+TerrelVerneuil@users.noreply.github.com>
2023-12-06 20:18:44 -0500

    Add files via upload

--- project files/dashboard.php
+++ project files/dashboard.php
@@ -25,51 +25,28 @@ if (isset($_GET['op'])) {
     $op = $_GET['op'];
 
     if ($op == 'login') {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $res = $db->query("SELECT * FROM shop_user WHERE name = '$username' AND password = '$password'");
-
-        if($res != FALSE){
-            $user = $res->fetch();
-            if($res -> rowCount() == 0){
-                loginAgain($db, "Username or password was incorrect. Please try again.", 'login');
-                exit;
-            }
-            else {
-                $_SESSION['uid'] = $user['userID'];
-                $op = $_POST['dest'];
-            }
-        }
-        else{
-            loginAgain($db, "An error has occurred. Please try again.", 'login');
-                exit;
-        }
+        
+        $_SESSION['uid'] = $_POST['uid'];
+        $op = $_POST['dest'];
     }
     else if ($op == 'logout') {
         unset($_SESSION['uid']);
     }
     else if ($op == 'signedUp') {
-        if($_POST['username'] == "" or $_POST['password'] == "" or $_POST['display_name'] == ""){
-            showTryAgain($db, "Please fill out all forms to create an account.");
-            exit;
+        $newUser = $_POST['username'];
+        $checkName = $db -> query("SELECT name
+                                  FROM shop_user
+                                  WHERE name = '$newUser'");
+        if($checkName -> rowCount() == 0){
+            $newPassword = $_POST['password'];
+            $db->query("INSERT INTO shop_user(name, created_at, password) VALUES ('$newUser', NOW(), '$newPassword')");
+            $getNewUID = $db -> query("SELECT userID FROM shop_user WHERE name = '$newUser'");
+            $newUID = $getNewUID -> fetch()['userID'];
+            $_SESSION['uid'] = $newUID;
         }
         else{
-            $newUser = $_POST['username'];
-            $checkName = $db -> query("SELECT name
-                                      FROM shop_user
-                                      WHERE name = '$newUser'");
-            if($checkName -> rowCount() == 0){
-                $newPassword = $_POST['password'];
-                $displayName = $_POST['display_name'];
-                $db->query("INSERT INTO shop_user(name, created_at, password, display_name) VALUES ('$newUser', NOW(), '$newPassword', '$displayName')");
-                $getNewUID = $db -> query("SELECT userID FROM shop_user WHERE name = '$newUser'");
-                $newUID = $getNewUID -> fetch()['userID'];
-                $_SESSION['uid'] = $newUID;
-            }
-            else{
-                showTryAgain($db, "That username already exists. Please try again.");
-                exit;
-            }
+            showTryAgain($db);
+            exit;
         }
     }
 }
@@ -78,15 +55,32 @@ $uid = $_SESSION['uid'];
 $uname = getName($db, $uid);
 
 if (isset($_GET['op']) && $_GET['op']  == 'showLoginForm'){
-    handleLoginForm($db, 'login');
-    exit;
+		handleLoginForm($db, 'login', "");
+   	 	exit;
 }
 
 if (isset($_GET['op']) && $_GET['op']  == 'showSignUpForm'){
     handleSignUpForm($db);
     exit;
 }
-
+if (isset($_GET['op']) && $_GET['op'] == 'showAccountManagement'){
+    showAccountManagementForm($db,$uid);
+    exit;
+}
+if (isset($_GET['op']) && $_GET['op'] == 'Account_Manage'){
+	$newPassword=$_POST['newPassword'] ?? null;
+	$confirmPassword = $_POST['confirmNewPassword']??null;
+	$currentPassword= $_POST['currentPassword']??null;	
+   $result = validateNewPassword($db,$uid,$currentPassword,$newPassword, $confirmPassword);     
+	
+	if($result == true){
+	   //update the database password based on the uid
+	   $db->query("UPDATE shop_user 
+		   SET password=$newPassword
+		WHERE userID='$uid'");
+   }
+   exit;
+}
 ?>
 
 <?php
@@ -137,67 +131,58 @@ else if($op == "addedToCart"){
         exit;
     }
     else{
-//        echo("<DIV class='main-content'><H1>" .$_GET['IID']. "$uid</H1>");
-        addToCart($db, $_GET['IID'], $uid);
+        showSellForm($db);
     }
-}
-else if($op == "cart"){
-    if(!isset($_SESSION['uid'])){
-        handleLoginForm($db, $op);
-        exit;
-    }
-    else{
-        if(isset($_POST['iid'])){
-            $removeItem = $_POST['iid'];
-            $db->query("DELETE FROM has_in_cart WHERE itemId = $removeItem AND userID = $uid");
-        }
-        showCart($db, $uid);
-    }
+    echo("HELLO");
 }
 else{
-    echo("<DIV class ='main-content'>\n");
-    echo("<H1>Picked for you</H1>");
-    if(isset($_SESSION['uid'])){
-        $randomItem = $db->query("SELECT * 
-                                  FROM item  
-                                  ORDER BY RAND()
-                                  LIMIT 10");
-        if($randomItem != FALSE){
-            for($x = 0; $x < 10; $x++){
-                $row = $randomItem -> fetch();
-                echo("<DIV class='item'>");
-                echo("<DIV class='item-box'>Null</DIV>");
-                echo("<DIV class='item-stack'>");
-                echo("<A href=?op=displayItem&IID=".$row['itemID'].">".$row['name']."</A>");
-                echo("<P>".$row['description']."</P>");
-                echo("<P>Price: $".$row['price']."</P>");
-                echo("</DIV>");
-                echo("</DIV>");
-        
-            }
-        }
-        
-    }
-    else{
-        echo("<P>Please sign in to see your recommended items.</P>");
-    }
-    echo("</DIV>");
+?>
+
+    <div class="main-content">
+    <h1>Picked for you</h1>
+    
+    <div class="item">
+        <div class="item-box">Null</div>
+        <p>Item Description</p>
+        <p>Item Price</p>
+    </div>
+    <div class="item">
+        <div class="item-box">Null</div>
+        <p>Item Description</p>
+        <p>Item Price</p>
+    </div>
+    <div class="item">
+        <div class="item-box">Null</div>
+        <p>Item Description</p>
+        <p>Item Price</p>
+    </div>
+    <div class="item">
+        <div class="item-box">Null</div>
+        <p>Item Description</p>
+        <p>Item Price</p>
+    </div>
+    
+</div>
+<?php    
 }
 ?>
 <DIV class="sidenav">
-<A href="?op=home"><H2>MARKETPLACE</H2></A>
+<H2>MARKETPLACE</H2>
 
 <?php
-	//this section icon should be pressed and prompt the user to a login page
-		//following that it should log the user in when user name and pass word is specified. 
-		//more functionality added later.
+//this section icon should be pressed and prompt the user to a login page
+	//following that it should log the user in when user name and pass word is specified. 
+	//more functionality added later.
 
 if (isset($_SESSION['uid'])) {
-    echo("👤 " . htmlspecialchars($uname));
-    showLogoutForm();
+
+?>
+	<a href="?op=showAccountManagement"><?php echo ("👤 " . htmlspecialchars($uname)) ?></a>
+<?php
+showLogoutForm();
 }
 else {
-    ?>
+?>
     <A id="username" href="?op=showLoginForm">👤 login</A>
     <A id="username" href="?op=showSignUpForm" style = 'color: blue; font-size: 10px'>Don't have an account? Click here to sign up!</A>
 <?php
@@ -206,18 +191,21 @@ else {
 
 	
 <DIV class="boxcontainer">
-    <DIV class="box"><A href="?op=cart">CART</A></DIV>
-            
+    <DIV class="box"><A href="?op=cart"><i style="font-size:24px" class="fa">&#xf07a;</i>
+</A></DIV>
+
     <DIV class="box"><A href="?op=sell">SELL</A></DIV>
+
     <DIV class="text-under-box"></DIV>
 </DIV>
 
 <DIV class="boxcontainer">
-    <DIV class="box"></DIV>
+    <DIV class="box"><A href="?op=saved"><i style="font-size:24px" class="fa">&#xf004;</i></A></DIV>
     <DIV class="text-under-box"></DIV>
-    <DIV class="box"></DIV>
+    <DIV class="box"><A href="?op=orders">ORDERS</A></DIV>
     <DIV class="text-under-box"></DIV>
 </DIV>
+
             
         
     <!-- search bar implement -->
@@ -257,3 +245,4 @@ if($op == 'filter'){
     
 </BODY>
 </HTML>
+
